cab48060b67f440ea80448420e99da5e8f1934d8
Roman Parpalak <roman@parpalak.com>
2023-05-26 23:05:22 +0300

    Updated translations and license info.

--- www/lang.en.php
+++ www/lang.en.php
@@ -41,50 +41,55 @@ return [
 			<div class="question">
 				<h3>What is “LaTeX”?</h3>
 				<p>
-					LaTeX is a computer typesetting system for complex documents.
+					LaTeX is a computer typesetting system for creating complex documents.
 					It is widely used in scientific publications, especially in physics and math.
-					<a href="http://en.wikipedia.org/wiki/LaTeX">See details in Wikipedia</a>.
+					For more details, <a href="http://en.wikipedia.org/wiki/LaTeX">refer to Wikipedia</a>.
 				</p>
 			</div>
 
 			<div class="question">
-				<h3>What does this website exactly do?</h3>
+				<h3>What does this website do exactly?</h3>
 				<p>
-					The website converts mathematical expressions into pictures ready for use in web.
-					You don’t have to create ones in a graphical editor and to upload somewhere.
-					Add pictures to discussions in blogs or forums or send links via messengers.
+					This website converts mathematical expressions into web-ready pictures.
+					You don’t need to create them in a graphical editor and to upload somewhere.
+					You can easily add pictures to discussions on blogs or forums, or share links via messengers.
 				</p>
 			</div>
 
 			<div class="question">
-				<h3>Is it for free?</h3>
-				<p>Yep. In case of reasonable load, of course. If you obstruct other users, your requests will be blocked.</p>
-			</div>
-
-			<div class="question">
-				<h3>How do I know if the service doesn’t stop working one day?</h3>
-				<p>You don’t. But I use it in my own projects and I’m not going to close it.</p>
-			</div>
-
-			<div class="question">
-				<h3>How is the math converted into pictures?</h3>
+				<h3>Is it free to use?</h3>
 				<p>
-					There is <a href="https://en.wikipedia.org/wiki/TeX_Live">Tex Live</a> installed on the server.
-					It has been set up to work with modern web technologies.
+					Yes, it is free to use, as long as there is a reasonable load on the service.
+					If your requests start to obstruct other users, your access may be blocked.
 				</p>
 			</div>
 
 			<div class="question">
-				<h3>How to include LaTeX packages? I’d like to write chemical formulas and musical notation!</h3>
-				<p>When processing pictures, only minimal package set is included. If there is a missing package,
-				please write me a letter. Don’t forget to explain how the package will help other users.</p>
+				<h3>Is there a guarantee that the service won’t stop working?</h3>
+				<p>No, there is no guarantee. However, I personally use it on my own websites and have no plans to shut it down.</p>
 			</div>
 
 			<div class="question">
-				<h3>Should I type math in this equation editor?</h3>
-				<p>Not necessarily. It’s convenient if you have only few formulas. Type large texts
-				in the <a href="https://upmath.me/">Upmath editor with LaTeX and Markdown support</a>.
-				Also, I’ve written a script for reach-math sites which allows you to use LaTeX directly in HTML source code.</p>
+				<h3>How are the formulas converted into images?</h3>
+				<p>
+					The server has <a href="https://en.wikipedia.org/wiki/TeX_Live">TeX Live</a> installed,
+					which works in conjunction with modern web technologies.
+				</p>
+			</div>
+
+			<div class="question">
+				<h3>Cyrillic and hieroglyphic characters are not displayed in the formulas!</h3>
+				<p>Perhaps the \text command can help you. For example, <code>Q_\text{плавления}>0</code> is displayed correctly: $$Q_\text{плавления}>0$$.</p>
+			</div>
+
+			<div class="question">
+				<h3>How to include LaTeX packages? I want images with chemical formulas and musical notations!</h3>
+				<p>When creating images, a minimal set of packages is included. If you need a specific package, please write me an email. Don’t forget to explain how the package will be useful to other users.</p>
+			</div>
+
+			<div class="question">
+				<h3>Do I have to type all formulas in this editor?</h3>
+				<p>Not necessarily. If you have only few formulas, it’s convenient to type them directly in the equation editor. For longer texts, you can use the <a href="https://upmath.me/">Upmath editor with LaTeX and Markdown support</a>. Additionally, I have developed a script for math-rich websites that enables you to use LaTeX directly in the HTML source code.</p>
 			</div>
 			</div>
 	',
@@ -93,31 +98,31 @@ return [
 
 			<div class="info-text">
 			<p>
-				Authors of mathematical text may write LaTeX expressions right into HTML code.
-				In order to be automatically converted, these expressions are to be written in dollar signs
-				<code><span>$$</span>...$$</code>, and the following script must be included:
+				Authors of mathematical text can write LaTeX expressions directly in HTML code.
+				To have them automatically converted, enclose the expressions within dollar signs
+				<code><span>$$</span>...$$</code>, and include the following script:
 			</p>
 	',
 	'embedding section 2'   => '
-			<p>Here is the HTML code sample:</p>
+			<p>Here is a sample of HTML code:</p>
 	',
 	'embedding section 2.1' => '
 			<p>The rendered result:</p>
 	',
 	'embedding section 3'   => '
 			<div class="question">
-				<p>In modern browsers, script loads pictures in vector format SVG and aligns formula baseline with surrounding text:</p>
+				<p>In modern browsers, the script loads pictures in vector format SVG and aligns the formula’s baseline with the surrounding text:</p>
 
 				<p align="center"><img src="/i/baseline_en.png" alt="" width="400" height="230" class="screenshot" style="max-width: 90vw; max-height: 51.75vw;" /></p>
 			</div>
 
 			<p>
-				My <a href="https://susy.page/">blog on theoretical physics</a> is based on this service.
+				This service powers my <a href="https://susy.page/">blog on theoretical physics</a>.
 			</p>
 			</div>
 	',
 	'copyright section'     => <<<TEXT
-				&copy; 2014&ndash;2020 <a href="https://parpalak.com/">Roman Parpalak</a>.
+				&copy; 2014&ndash;2023 <a href="https://parpalak.com/">Roman Parpalak</a>.
 				<script>var mailto="roman%"+"40parpalak.com";document.write('Drop&nbsp;me&nbsp;a&nbsp;line: <a href="mailto:'+unescape(mailto)+'">' + unescape(mailto) + '</a>.');</script>
 				&nbsp; &nbsp;
 				<div class="likely" data-url="https://i.upmath.me/">
